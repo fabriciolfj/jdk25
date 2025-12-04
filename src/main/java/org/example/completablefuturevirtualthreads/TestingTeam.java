fae5272423181b61814ca611323b6a7c605b60ac
fabricio <fabricio.jacob@outlook.com>
2025-12-03 21:04:26 -0300

    add

---
+++ src/main/java/org/example/completablefuturevirtualthreads/TestingTeam.java
@@ -0,0 +1,3 @@
+package org.example.completablefuturevirtualthreads;
+
+public record TestingTeam(String... testers) {}
---
+++ src/main/java/org/example/completablefuturevirtualthreads/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package org.example.completablefuturevirtualthreads;
+
+public class UserNotFoundException extends RuntimeException {
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
