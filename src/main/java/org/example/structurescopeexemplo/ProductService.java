d8b17febec3c7149080e7038b8e5914fb9dac286
spark <fabricio.jacob@outlook.com>
2026-02-04 21:05:42 -0300

    feat: add

--- src/main/java/org/example/structurescopeexemplo/ProductService.java
+++ src/main/java/org/example/structurescopeexemplo/ProductService.java
@@ -1,32 +1,29 @@
 package org.example.structurescopeexemplo;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.StructuredTaskScope;
 
+@SuppressWarnings("ALL")
 public class ProductService {
 
-    public ProductInfo fetchProductInfo(final Long productId) {
+    public ProductInfo fetchProductInfo(final Long productId, boolean shouldFail) {
+        Instant start = Instant.now();
         log("fetching product & reviews for id: " + productId);
 
-        try(var service = Executors.newVirtualThreadPerTaskExecutor()) {
-            Future<Product> productTask = service.submit(() -> fetchProduct(productId));
-            Future<List<Review>> reviewsTask = service.submit(() -> fetchReviews(productId));
+        try(var scope = StructuredTaskScope.open(StructuredTaskScope.Joiner.anySuccessfulResultOrThrow())) {
+            StructuredTaskScope.Subtask<Product> productTask = scope.fork(() -> shouldFail ? fetchProductThatFails(productId) : fetchProduct(productId));
+            StructuredTaskScope.Subtask<List<Review>> reviewsTask = scope.fork(() -> fetchReviews(productId));
 
-            Product product = productTask.get();
-            log("product retrieved for id: " + productId);
+            log("... scope joining. Waiting for substasks");
+            scope.join();
+            log("...Scioe joined successfuly");
 
-            var reviews = reviewsTask.get();
-            log("reviews retrieved for id: " + productId);
-            log("all info fetched for id: " + productId);
-
-            return new ProductInfo(product, reviews);
-        } catch (ExecutionException | InterruptedException e) {
+            return new ProductInfo(productTask.get(), reviewsTask.get());
+        } catch (InterruptedException e) {
             Throwable cause = e.getCause() != null ? e.getCause() : e;
             log("error processing product info for id: " + productId + ": " + cause.getMessage());
 
@@ -35,7 +32,45 @@ public class ProductService {
             }
 
             throw new ProductServiceException("fetch failed for id: " + productId, cause);
+        } finally {
+            Instant end = Instant.now();
+            log("total time taken: " + Duration.between(start, end).toMillis() + "ms");
         }
+    }
+
+    public Product fetchProductTest(long productId) {
+        try(var scope = StructuredTaskScope.open(StructuredTaskScope.Joiner.<Product>anySuccessfulResultOrThrow())) {
+            scope.fork(() -> fetchProductFromDataBase(productId));
+            scope.fork(() -> fetchProductFromCache(productId));
+            scope.fork(() -> fetchProductFromApi(productId));
+
+            return scope.join();
+        } catch (InterruptedException e) {
+            log("error processing fetch product " + e.getMessage());
+            throw new ProductServiceException(e.getMessage());
+        }
+    }
+
+    private Product fetchProductFromCache(long productId) throws InterruptedException {
+        //throw new RuntimeException();
+        log("-> checking cache... (will take 500ms)");
+        sleepForAWhile(Duration.ofMillis(500));
+        log("-> cache has the result!");
+        return new Product(productId, "product from cache", "");
+    }
+
+    private Product fetchProductFromDataBase(long productId) throws InterruptedException {
+        log("-> checking database... (will take 500ms)");
+        sleepForAWhile(Duration.ofSeconds(2));
+        log("-> cache has the result!");
+        return new Product(productId, "product from database", "");
+    }
+
+    private Product fetchProductFromApi(long productId) throws InterruptedException {
+        log("-> checking api... (will take 500ms)");
+        sleepForAWhile(Duration.ofSeconds(3));
+        log("-> cache has the result!");
+        return new Product(productId, "product from api", "");
     }
 
     private Product fetchProduct(Long productId) {
@@ -53,6 +88,11 @@ public class ProductService {
         return new Product(productId, "sample product", "this grat product description");
     }
 
+    private Product fetchProductThatFails(long productId) {
+        log("-> fetching products details... (will fail)");
+        throw new ProductServiceException("product id " + productId + " not found");
+    }
+
     private List<Review> fetchReviews(Long productId) {
         log("fetching reviews for id: " + productId);
         if (productId == 1L) {
@@ -62,6 +102,7 @@ public class ProductService {
         }
 
         log("reviews for id: " + productId + " -simulating network call (2 seconds).");
+        sleepForAWhile(Duration.ofSeconds(2));
         List<Review> reviews = List.of(
                 new Review(1L, "excellent!", 5, productId),
                 new Review(2L, "good value!", 4, productId)
@@ -88,10 +129,12 @@ public class ProductService {
 
     static void main() {
         ProductService productService = new ProductService();
-        long testProductId = 2L;
-        log("attempting to fetch product info for ID: " + testProductId);
+
+        /*long testProductId = 2L;
+        log("attempting to fetch product info for ID: " + testProductId);*/
         try {
-            ProductInfo productInfo = productService.fetchProductInfo(testProductId);
+            //ProductInfo productInfo = productService.fetchProductInfo(testProductId, false);
+            IO.println(productService.fetchProductTest(2L));
         } catch (ProductServiceException e) {
             log("service error: " + e.getMessage() + (e.getCause() != null ? " | Caused by: " + e.getCause().getMessage() : ""));
         }
